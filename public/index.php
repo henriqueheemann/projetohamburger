d29ccf3eec6876b04afba2e9ea8a4b0fe2052c51
Your Name <you@example.com>
2019-10-03 17:05:05 -0300

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,3 +1,4 @@
+
 <?php
 if (PHP_SAPI == 'cli-server') {
     // To help the built-in PHP dev server, check if the request was actually for
@@ -8,26 +9,21 @@ if (PHP_SAPI == 'cli-server') {
         return false;
     }
 }
-
 require __DIR__ . '/../vendor/autoload.php';
-
 session_start();
-
 // Instantiate the app
 $settings = require __DIR__ . '/../src/settings.php';
 $app = new \Slim\App($settings);
-
 // Set up dependencies
 $dependencies = require __DIR__ . '/../src/dependencies.php';
 $dependencies($app);
-
 // Register middleware
 $middleware = require __DIR__ . '/../src/middleware.php';
 $middleware($app);
-
+// Register routes
+$routes = require __DIR__ . '/../src/routes.php';
+$routes($app);
 $routes = require __DIR__ . '/../src/routesLogin.php';
 $routes($app);
-
 // Run app
 $app->run();
-
