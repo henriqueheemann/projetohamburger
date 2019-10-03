4b9d270004713bb3551ace3e646184ae239579bc
Your Name <you@example.com>
2019-10-03 16:52:48 -0300

    Update routesLogin.php

--- src/routesLogin.php
+++ src/routesLogin.php
@@ -1,12 +1,9 @@
 <?php
-
 use Slim\App;
 use Slim\Http\Request;
 use Slim\Http\Response;
-
 return function (App $app) {
     $container = $app->getContainer();
-
     $app->get('/login/', function (Request $request, Response $response, array $args) use ($container) {
         // Sample log message
         $container->get('logger')->info("Slim-Skeleton '/login/' route");
@@ -27,6 +24,7 @@ return function (App $app) {
             echo "ACESSO NEGADO";
             exit;
         }
+        // Render index view
         return $container->get('renderer')->render($response, 'login.phtml', $args);
     });
 };
