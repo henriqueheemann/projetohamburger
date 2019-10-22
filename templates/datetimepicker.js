932e30a84f3807f5b81e40d3a1221203a893053d
Your Name <you@example.com>
2019-10-22 16:31:43 -0300

    Create datetimepicker.js

---
+++ templates/datetimepicker.js
@@ -0,0 +1,8 @@
+$( document ).ready(function() {
+    $(function () {
+        $('#datetimepicker13').datetimepicker({
+            inline: true,
+            sideBySide: true
+        })
+    });
+});
