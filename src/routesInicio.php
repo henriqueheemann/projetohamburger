b23e25e253d23ff5f7aefd43dd3ba0e884d66ea3
henriqueheemann <henrique_heemann@estudante.sc.senai.br>
2019-10-27 22:31:15 -0300

    Update routesInicio.php

--- src/routesInicio.php
+++ src/routesInicio.php
@@ -10,15 +10,7 @@ return function (App $app) {
     $app->get('/inicio/[{sucesso}]', function (Request $request, Response $response, array $args) use ($container) {
         // Sample log message
         $container->get('logger')->info("Slim-Skeleton '/inicio/' route");
-
-        $conexao = $container->get('pdo');
-        $resultSet = $conexao->query('SELECT * FROM inicio')->fetchAll();
         
-
-        $args['textos'] = $resultSet;
-        
-
-
         // Render index view
         return $container->get('renderer')->render($response, 'inicio.phtml', $args);
     });
