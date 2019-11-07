a7b082db08643a89c662ea52a9b93c669598ed30
Your Name <you@example.com>
2019-11-07 17:22:09 -0200

    produtoSelec andamento

--- src/routesSuplemento.php
+++ src/routesSuplemento.php
@@ -7,17 +7,18 @@ use Slim\Http\Response;
 return function (App $app) {
     $container = $app->getContainer();
 
+
     $app->get('/suplemento/', function (Request $request, Response $response, array $args) use ($container) {
         // Sample log message
         $container->get('logger')->info("Slim-Skeleton '/suplemento/' route");
 
 
-        $conexao= $container->get('pdo');
+        $conexao = $container->get('pdo');
 
         $resultSet = $conexao->query('SELECT * FROM produto')->fetchAll();
 
         $args['produtos'] = $resultSet;
-        
+
 
 
         // Render index view
@@ -48,11 +49,15 @@ return function (App $app) {
             $_SESSION['produto'][] = $resultSet;
         }
 
-
         return $container->get('renderer')->render($response, 'suplemento_selec.phtml', $args);
     });
+    $app->get('/limparcarrinho/', function (Request $request, Response $response, array $args) use ($container) {
+        // Sample log message
+        $container->get('logger')->info("Slim-Skeleton '/limparcarrinho/' route");
 
+        unset($_SESSION['produto']);
+        
+        session_destroy();
+        return $response->withRedirect('/suplemento/');
+    });
 };
-
-
-
