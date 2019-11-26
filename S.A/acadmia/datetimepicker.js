cee854fb897602061c8ac61cca83ef12bdc9ac76
Your Name <you@example.com>
2019-11-26 15:14:36 -0200

    Commit mudanças

--- S.A/acadmia/datetimepicker.js
+++
@@ -1,8 +0,0 @@
-$( document ).ready(function() {
-    $(function () {
-        $('#datetimepicker13').datetimepicker({
-            inline: true,
-            sideBySide: true
-        })
-    });
-});
